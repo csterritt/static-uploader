1f0564807ec80b2bfba88e360ae48b8f13ea71aa
csterritt <csterritt@gmail.com>
2023-01-25 10:28:32 -0500

    Now finds all local files below the given subdirectory.

--- main.go
+++ main.go
@@ -2,24 +2,18 @@ package main
 
 import (
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
 	"os"
 )
 
-var neededVars = []string{"STORAGE_KEY_ID", "STORAGE_KEY_CONTENT", "STORAGE_BUCKET_NAME", "STORAGE_BUCKET_ENDPOINT"}
+var neededEnvVars = []string{"STORAGE_KEY_ID", "STORAGE_KEY_CONTENT", "STORAGE_BUCKET_NAME", "STORAGE_BUCKET_ENDPOINT"}
 
 func isNotDir(dir string) bool {
-	// This returns an *os.FileInfo type
 	fileInfo, err := os.Stat(dir)
 	if err != nil {
 		fmt.Printf("Cannot stat directory %s: %s\n", dir, err)
 		return true
 	}
 
-	// IsDir is short for fileInfo.Mode().IsDir()
 	if !fileInfo.IsDir() {
 		fmt.Printf("File %s is not a directory\n", dir)
 		return true
@@ -30,7 +24,7 @@ func isNotDir(dir string) bool {
 
 func verifyEnvironment() {
 	fail := false
-	for _, name := range neededVars {
+	for _, name := range neededEnvVars {
 		val := os.Getenv(name)
 		if val == "" {
 			fail = true
@@ -48,46 +42,50 @@ func verifyEnvironment() {
 	}
 }
 
-func main() {
-	verifyEnvironment()
-
-	bucket := aws.String(os.Getenv("STORAGE_BUCKET_NAME"))
-	//key := aws.String("fileName.txt")
-
-	s3Config := &aws.Config{
-		Credentials:      credentials.NewStaticCredentials(os.Getenv("STORAGE_KEY_ID"), os.Getenv("STORAGE_KEY_CONTENT"), ""),
-		Endpoint:         aws.String("https://" + os.Getenv("STORAGE_BUCKET_ENDPOINT")),
-		Region:           aws.String("us-east-1"),
-		S3ForcePathStyle: aws.Bool(true),
-	}
-	//newSession := session.New(s3Config)
-	newSession, err := session.NewSession(s3Config)
+func findAllFiles(localDir string) ([]string, error) {
+	dirs, err := os.ReadDir(localDir)
 	if err != nil {
-		fmt.Printf("Failed to create session %s\n", err.Error())
+		fmt.Printf("Cannot read files in %s: %v\n", localDir, err)
+		return nil, err
 	}
 
-	s3Client := s3.New(newSession)
+	res := make([]string, 0)
+	for _, file := range dirs {
+		if file.IsDir() {
+			subFiles, err := findAllFiles(localDir + "/" + file.Name())
+			if err != nil {
+				return nil, err
+			}
 
-	// List files
-	fmt.Printf("bucket: %s\n", *bucket)
-	data, err := s3Client.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: bucket})
-	if err != nil {
-		fmt.Printf("ListObjectsV2 got error %v\n", err)
-	} else {
-		// fmt.Printf("ListObjectsV2 returned data %#v\n", data)
-		for _, content := range data.Contents {
-			fmt.Printf("File: %s\n", *content.Key)
+			res = append(res, subFiles...)
+		} else {
+			res = append(res, localDir+"/"+file.Name())
 		}
 	}
 
-	// Delete an existing object
-	//_, err = s3Client.DeleteObject(&s3.DeleteObjectInput{
-	//	Bucket: bucket,
-	//	Key:    key,
-	//})
-	//if err != nil {
-	//	fmt.Printf("Failed to delete object %s/%s, %s\n", *bucket, *key, err.Error())
-	//}
+	return res, nil
+}
+
+func copyLocalToBucket(localDir string, bucket string) {
+	localFiles, err := findAllFiles(localDir)
+	if err != nil {
+		fmt.Printf("Cannot read localFiles in %s: %v\n", localDir, err)
+		os.Exit(1)
+	}
+
+	dirLen := len(localDir) + 1
+	for _, file := range localFiles {
+		fmt.Printf("%s\n", file[dirLen:])
+	}
+}
+
+func main() {
+	verifyEnvironment()
+
+	bucket := os.Getenv("STORAGE_BUCKET_NAME")
+	localDir := os.Args[1]
+
+	copyLocalToBucket(localDir, bucket)
 
 	fmt.Printf("Done!\n")
 }
